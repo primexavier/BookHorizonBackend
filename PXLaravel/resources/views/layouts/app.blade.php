15e6e97bf8ead8a7b2f620e4c6e86519b4efea44
Aldi Afandi <mraldiafandi@gmail.com>
2020-12-08 16:33:05 +0700

    update

--- PXLaravel/resources/views/layouts/app.blade.php
+++ PXLaravel/resources/views/layouts/app.blade.php
@@ -28,6 +28,8 @@
         <link rel="stylesheet" href="{{ mix('css/core-ui.css') }}">
 
         @livewireStyles
+        
+        @stack('styles')
 
         <!-- Scripts -->
         <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.7.3/dist/alpine.js" defer></script>
